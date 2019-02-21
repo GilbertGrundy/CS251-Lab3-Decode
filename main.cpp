76b35d56dca80af874314b8149ee8b99c66eb47f
Gilbert E F Grundy <46936337+GilbertGrundy@users.noreply.github.com>
2019-02-20 21:26:07 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,15 @@ using namespace std;
 int main(int argc, char ** argv)
 {
 	cout << "Gilbert Grundy CS251 Lab #3.\n";
-
+    	
+	//checks if there is the correct amount of arguments
+    	//there should just be one file passed in.
+    	if(argc != 2)
+    	{
+        	fprintf(stderr, "Codebreaker: Invalid amount of arguments.\n");
+        	return -1;
+    	}
+	
+	
 	return 0;
 }
