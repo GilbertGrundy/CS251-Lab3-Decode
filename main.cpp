db92ec8a33b5ad0819950fc4dfc91aff7a0b0ccf
GilbertGrundy <gilbert.grundy@gmail.com>
2019-02-20 21:23:35 -0800

    Main.cpp - added opening text.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ Assignment: Lab #3
 #include <iostream>
 #include <cstring>
 
-int main()
+using namespace std;
+
+int main(int argc, char ** argv)
 {
+	cout << "Gilbert Grundy CS251 Lab #3.\n";
+
+	return 0;
 }
