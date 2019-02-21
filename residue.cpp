0436be845adf514b5cd5df70ef93aaf4fe1eab5c
GilbertGrundy <gilbert.grundy@gmail.com>
2019-02-20 21:18:25 -0800

    Edited residue.cpp for this lab.

--- residue.cpp
+++ residue.cpp
@@ -1,86 +1,29 @@
-/*
-Name: Gilbert Grundy
-Assignment: Lab #2
-*/
-
 #include <iostream>
 #include <cstring>
 
-int get_values(long long &a, long long &k, long long &n);
+long long residue(int num, int mod, long long exponent);
 void find_residue(int &count, long long &a, long long k, long long n, long long &residue, long long &total);
 long long next_res(long long residue, long long mod);
-bool repeat();
 
 using namespace std;
 
-int main()
+
+long long residue(long long num, long long mod, long long exponent)
 {
-	long long integer = 0;	//integer to mod
-	long long exponent = 0;	//number to be exponent to a - a^k 
-	long long mod = 0;	//number to modulus
 	long long residue = 0;	//residue of a^k mod n
 	long long total = 1;	//sum of a^(2^a1, a2, ... an) mod n
 	int count = 0;	// count the exponent k of a^(2^k)
 
-	cout << "Gilbert Grundy CS251 Lab #2.\n";
 
-	cout << "Let's calculate the residue of the form a^k MOD n.\n\n";
-
-	do
-	{	//function to collect values from user
-		get_values(integer, exponent, mod);
-
-		//captures the residue of a^1 mod n
-		residue = (integer % mod);
+	//captures the residue of a^1 mod n
+	residue = (num % mod);
 	
-		//solve the residue	
-		find_residue(count, integer, exponent, mod, residue, total);
+	//solve the residue	
+	find_residue(count, num, exponent, mod, residue, total);
 
-		//states entered  values
-		cout << "\n\n" << integer << "^" << exponent << " MOD " << mod << " = ";
-		
-		//display the answer by the residue mod n
-		cout << (total%mod) << endl << endl;
-
-		//reset the values
-		residue = 0;
-		total = 1;
-		count = 0;
-
-	}while(repeat()); //check if user wants to repeat
-
-	return 0;
+	return (total%mod);
 }
 
-
-//function to get values from user
-int get_values(long long &a, long long &k, long long &n)
-{
-	cout << "Please enter a vlaue for a: ";
-
-	cin >> a;
-	cin.clear();
-	cin.ignore(100, '\n');
-
-	cout << "Please enter a value for k: ";
-
-	cin >> k;
-	cin.clear();
-	cin.ignore(100, '\n');
-	
-	cout << "Please enter a value for n: ";
-
-	cin >> n;
-	cin.clear();
-	cin.ignore(100, '\n');
-
-	cout << endl;
-
-	if(n == 0) return -1;
-	if(k == 0) return 0;
-
-	return 1;
-}
 
 
 //function to find final residue of a^k mod n
@@ -88,19 +31,11 @@ void find_residue(int &count, long long &a, long long k, long long n, long long 
 {
 	if(k < 1) return; //leaves if can't be divided anymore.
 
-	//display step:
-	cout << a << "^(2^" << count << ") MOD " << n << " = " << residue;
-
 	//if k%2 = 1, then a^(2^k) mod n is relevant
-	if(k % 2) 
-	{
-		total = (total * (residue%n));
-		cout << "*";
-	}
+	if(k % 2) total = (total * (residue%n));
+
 	//get the value of a^(2^(k+1)) mod n
 	residue = next_res(residue, n);
-
-	cout << endl;
 
 	//halve the exponent, count the exponent, call the function recursively
 	find_residue(++count, a, (k/2), n, residue, total);
@@ -117,24 +52,4 @@ void find_residue(int &count, long long &a, long long k, long long n, long long 
 long long next_res(long long residue, long long mod)
 {
 	return ((residue*residue)%(mod));
-}
-
-
-//function to check if the user would like to go again
-bool repeat()
-{
-	char again;
-	bool repeat = true;
-	do
-	{
-		cout << "Would you like to run the program again? y/n: ";
-
-		cin >> again;
-		cin.clear();
-		cin.ignore(100, '\n');
-		cout << "\n\n";
-
-		if(again == 'y' || again == 'Y') return true;
-		if(again == 'n' || again == 'N') return false;
-	}while(repeat);
 }
